9169f9dc1f60df74abd34937c46bc4375bf2d25e
Robot-Boy-kuo <kuo.shengkun@outlook.com>
2022-09-05 13:47:13 +0800

    更新前端框架

--- vue/src/main.js
+++ vue/src/main.js
@@ -5,6 +5,8 @@ import router from './router'
 import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
 
+import './assets/global.css'
+
 Vue.use(ElementUI,{size:"small"});
 
 Vue.config.productionTip = false
